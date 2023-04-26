2b4d05e0aa4f597f128a06ab2e73be553e91c210
kuvinci <kuvinci@gmail.com>
2023-04-26 12:53:41 +0300

    fixing gitignore

--- src/keys/keys.dev.ts
+++ src/keys/keys.dev.ts
@@ -3,3 +3,4 @@ export const SESSION_SECRET: string = 'MFVdZOA.A5AWzOk59o-rrpRGH6jNj0';
 export const SENDGRID_API: string = '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';
 export const EMAIL_FROM: string = 'akuzub@access.dev';
 export const BASE_URL: string = 'http://localhost:3000';
+
